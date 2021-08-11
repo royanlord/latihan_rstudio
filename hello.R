1e29800af3c8cf7e3b5f9b87868a2e50f6a50825
royanlord <royanfarid32@gmail.com>
2021-08-11 21:37:46 +0700

    initial commit

--- hello.R
+++ hello.R
@@ -1,8 +1,10 @@
 teks1 = "Selamat datang di dunia R"
 teks2 = "Hello world!"
+teks3 = "Perkenalkan namaku Royan Farid..."
 
 teks1
 teks2
+teks3
 
-list1 = list("Ngegame", "Belajar", "Rebahan")
-list1
+hobi1 = list("Ngegame", "Belajar", "Rebahan")
+hobi1
